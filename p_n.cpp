776bac4cfaadce3a4a76bfb40914d18e2b05c7c9
Mohit Pal <116500860+mohitpal24@users.noreply.github.com>
2023-04-29 23:07:27 +0530

    Create p_n.cpp

---
+++ p_n.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    if(n>=0){
+        cout<<"The number is positive";
+    }
+    else{
+        cout<<"the number is negative";
+    }
+    return 1;
+}
