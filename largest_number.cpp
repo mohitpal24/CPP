c36f7a9244f7a4b7a4900d8eaf6524a03f437968
Mohit Pal <116500860+mohitpal24@users.noreply.github.com>
2023-04-29 21:38:11 +0530

    Create largest_number.cpp

---
+++ largest_number.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+    cin >> a;
+    cin >> b;
+    cin >> c;
+    if (a > b && a > c)
+    {
+        cout << a << " is greater";
+    }
+    else if (b > a && b > c)
+    {
+        cout << b << " is greater";
+    }
+    else
+    {
+        cout << c << " is greater";
+    }
+
+    return 1;
+}
