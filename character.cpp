b99d7667c310bdb24ce239f2ad094a7d072d921f
Mohit Pal <116500860+mohitpal24@users.noreply.github.com>
2023-04-29 23:12:27 +0530

    Create character.cpp

---
+++ character.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    char a;
+    cin>>a;
+    if(a>='a' && a<='z' || a>='A'&& a<='Z'){
+        cout<<"the character is alphabet";
+
+    }
+    else{
+        cout<<"the character is not alphabet";
+    }
+    return 1;
+}
