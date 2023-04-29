11018a0d7e28b44989046f91c4de88091be7d3b5
Mohit Pal <116500860+mohitpal24@users.noreply.github.com>
2023-04-29 23:04:17 +0530

    Create leap_year.cpp

---
+++ leap_year.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int year;
+    cin >> year;
+    if (year % 4 == 0)
+    {
+        cout << "This Year is A Leap Year";
+    }
+    else
+    {
+        cout << "This year is not a Leap Year";
+    }
+    return 1;
+}
